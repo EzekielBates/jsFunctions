fa5966f35419d832f15405af7212947668c40dfe
BATES <eb0888960@otc.edu>
2018-09-05 09:23:36 -0500

    Finished Assignment

---
+++ min.js
@@ -0,0 +1,15 @@
+function min(x,y){
+    if(x>y){
+        return y;
+    }
+    else if(x<y){
+        return x;
+    }
+    else {
+        return x;
+    }
+}
+
+console.log(min(4.5, 5)); // Must show 4.5
+console.log(min(19, 9));  // Must show 9
+console.log(min(1, 1));   // Must show 1
---
+++ square.js
@@ -0,0 +1,11 @@
+// Square the given number x
+function square1(x) {
+  return x*x;
+}
+
+// Square the given number x
+const square2 = x => {return x*x;}// TODO: complete the function code
+
+for(let i = 0;  i <= 10; ++i){
+    console.log(square1(i));
+}
